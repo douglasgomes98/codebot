bba756da0ceeab1396a825d67b40b7946c236eca
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-08-16 12:21:15 -0400

    create initial commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codebot" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "codebot.generateCode",
+    async (args) => {
+      const codeName = await vscode.window.showInputBox({
+        prompt: `Enter the code name:`,
+        ignoreFocusOut: true,
+        valueSelection: [-1, -1],
+      });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codebot.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from codebot!');
-	});
+      console.log(args);
+      console.log(codeName);
+      console.log(__dirname);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
