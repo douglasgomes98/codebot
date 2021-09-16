3a6e6eb799098ae693c8e7db90a6cd5cd684f496
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-09-16 02:06:17 -0400

    feat: add default template type

--- src/extension.ts
+++ src/extension.ts
@@ -21,11 +21,13 @@ export function activate(context: vscode.ExtensionContext) {
           throw new Error('Invalid code name!');
         }
 
-        const templateType = await vscode.window.showInputBox({
+        const templateTypeByUser = await vscode.window.showInputBox({
           prompt: 'Enter the template type:',
           ignoreFocusOut: true,
           valueSelection: [-1, -1],
         });
+
+        const templateType = templateTypeByUser || 'Component';
 
         if (!templateType) {
           throw new Error('Invalid template type!');
