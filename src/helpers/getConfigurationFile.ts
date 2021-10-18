e0b6f9b5a7cf46c7bae7a2b37881a921ed0e3fa3
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-10-18 19:01:54 -0400

    fix: error on not loader configuration file

--- src/helpers/getConfigurationFile.ts
+++ src/helpers/getConfigurationFile.ts
@@ -4,17 +4,21 @@ import { ConfigurationFile } from '../types';
 import { getWorkspaceFolder } from './getWorkspaceFolder';
 
 export function getConfigurationFile(): ConfigurationFile | null {
-  const workspaceFolderPath = getWorkspaceFolder();
+  try {
+    const workspaceFolderPath = getWorkspaceFolder();
 
-  if (workspaceFolderPath) {
-    const file = fs.readFileSync(
-      path.resolve(workspaceFolderPath, 'codebot.config.json'),
-      'utf-8',
-    );
+    if (workspaceFolderPath) {
+      const file = fs.readFileSync(
+        path.resolve(workspaceFolderPath, 'codebot.config.json'),
+        'utf-8',
+      );
 
-    const configurationFile = JSON.parse(file);
-    return configurationFile;
+      const configurationFile = JSON.parse(file);
+      return configurationFile;
+    }
+
+    return null;
+  } catch (error) {
+    return null;
   }
-
-  return null;
 }
